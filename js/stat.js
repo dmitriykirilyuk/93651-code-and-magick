9d5e778c13d7eb739f69991ce5fb631cc6217bce
dmitriykirilyuk <dimasishere@gmail.com>
2017-01-27 02:25:12 +0200

    Закончил гистограмму

--- js/stat.js
+++ js/stat.js
@@ -2,20 +2,48 @@
 
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, 420, 300);
 
   ctx.fillStyle = "#ffffff";
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 300);
 
   ctx.fillStyle = "#000000"
   ctx.font = "16px PT Mono";
   ctx.fillText("Ура вы победили!", 120, 40);
   ctx.fillText("Список результатов:", 120, 65);
+
+  var max = -1;
+  for(var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    };
+  };
+
+  var histoHeight = 150;
+  var histoX = 150;
+  var step = histoHeight / max;
+  var columnIndent = 50;
+
+  for (var i = 0; i < times.length; i++) {
+    var name = names[i];
+    var time = times[i];
+
+    var height = step * time;
+
+    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90);
+
+    if (name === "Вы!") {
+      ctx.fillStyle = "rgba(255, 0, 0, 1)";
+    } else {
+      ctx.fillStyle = ["rgba(21, 96, 189, ",(Math.random()).toFixed(1),")"].join("");
+    };
+    ctx.fillRect (histoX + columnIndent * i, 100, 40, height);
+
+    ctx.fillStyle ="purple";
+    ctx.fillText(name, histoX + columnIndent * i, 100 + histoHeight + 20);
+  };
 };
 
-
-var histoHeight = 150;
-var histoWidth = 40;
-
 var canvas = document.querySelector("canvas");
-window.renderStatistics(canvas.getContext("2"), ["Дмитрий", "Атнон", "Я"], [7.22, 18.44, 26.11]);
+window.renderStatistics(canvas.getContext("2d"), ["Аня", "Коля", "Вы!", "Юра", "Сеня"], [7.22, 18.44, 26.11, 11.22, 15.44]);
